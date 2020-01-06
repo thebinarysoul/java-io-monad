73552886afdaf860a0766fecf0cc33d52b411b3c
thebinarysoul <willymyfriend@gmail.com>
2020-01-06 08:02:14 +0300

    changed name of method in test

--- src/test/java/thebinarysoul/io/RefTransparencyTest.java
+++ src/test/java/thebinarysoul/io/RefTransparencyTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class RefTransparencyTest {
     int externalVariable = 0;
 
-    private int addAndIncrementSideEffect(final int a, final int b){
+    private int addAndIncrementExtVar(final int a, final int b){
         final var result = a + b;
         externalVariable += 1;
         return result;
@@ -15,11 +15,11 @@ public class RefTransparencyTest {
 
     @Test
     public void willNotBeReferentiallyTransparent() {
-        final var sumOfTwiceMethodCall = addAndIncrementSideEffect(1, 1) + addAndIncrementSideEffect(1, 1);
+        final var sumOfTwiceMethodCall = addAndIncrementExtVar(1, 1) + addAndIncrementExtVar(1, 1);
         final var extVarByCallingMethodTwice = externalVariable;
         externalVariable = 0;
 
-        final var value = addAndIncrementSideEffect(1, 1);
+        final var value = addAndIncrementExtVar(1, 1);
         final var sumOfValues = value + value;
         final var extVarByConvertCallToValue = externalVariable;
         externalVariable = 0;
@@ -30,14 +30,14 @@ public class RefTransparencyTest {
 
     @Test
     public void willBeReferentiallyTransparent() {
-        final var sumOfTwiceMethodCall = IO.apply(() -> addAndIncrementSideEffect(1, 1))
-                                                 .map(x -> x + addAndIncrementSideEffect(1, 1))
+        final var sumOfTwiceMethodCall = IO.apply(() -> addAndIncrementExtVar(1, 1))
+                                                 .map(x -> x + addAndIncrementExtVar(1, 1))
                                                  .unsafeRun();
 
         final var extVarByCallingMethodTwice = externalVariable;
         externalVariable = 0;
 
-        final var value = IO.apply(() -> addAndIncrementSideEffect(1, 1));
+        final var value = IO.apply(() -> addAndIncrementExtVar(1, 1));
         final var sumOfValues = value.flatMap(x -> value.map(y -> x + y)).unsafeRun();
         final var extVarByConvertCallToValue = externalVariable;
         externalVariable = 0;
